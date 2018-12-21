33df2c4eb0ff95c1c19dbe19ad3c682fd64542c1
parkermuir <parker.muir@gmail.com>
2018-12-21 15:08:54 -0500

    SB tree set up and action list configuration

--- DM50/DM50.py
+++ DM50/DM50.py
@@ -7,12 +7,12 @@ from os import listdir
 import pandas as pd
 #import time
 
-def get_filenames(path, suffix=".csv"):
+def get_filenames(path, suffix='.csv'):
     filenames = listdir(path)
     return [filename for filename in filenames if filename.endswith(suffix)]
 
 def get_dataframes(filenames, path):
-    print "loading files from " + path
+    print 'loading files from ' + path
     #creates a dictionary that holds a dataframe of each csv
     actions_dict = {}
     
@@ -21,7 +21,7 @@ def get_dataframes(filenames, path):
         keep_col = ['Combo', 'Weight']
         file_df = file_df[keep_col]
 
-        action_name = filename.replace('50bb_',"").replace('.csv',"")
+        action_name = filename.replace('50bb_','').replace('.csv','')
         actions_dict[action_name] = file_df
     
     return actions_dict
@@ -36,7 +36,7 @@ def match_dataframes(handlist, df_dict):
             match_df = df[df['Combo'] == h]
 
             if not match_df.empty:
-                match_dict[key] = match_df.iloc[0,1]
+                match_dict[key] = round(match_df.iloc[0,1], 3)
                 break
             else:
                 next
@@ -50,10 +50,10 @@ def RNG():
    return r    
     
 def convertinput(userinput):
-    finalhand = "not recognized" 
+    finalhand = 'not recognized' 
     
     if (len(userinput) > 8 or len(userinput) < 4):
-        finalhand = "not recognized" 
+        finalhand = 'not recognized' 
         pass
     else:
         input0 = userinput[0:1]
@@ -66,44 +66,44 @@ def convertinput(userinput):
         input7 = userinput[7:8]
 
     if len(userinput) == 4:
-        finalhand = "%sd%ss%sh%sc" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+        finalhand = '%sd%ss%sh%sc' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
     elif (len(userinput) == 5 or len(userinput) == 6):
     #for 5 or 6 character entries only
         if input0 == input1:  #check if first two characters are a pair and do the suiting this way
-            if input4 == "s":
-                finalhand = "%sc%sh%sh%sd" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-            elif input4 == "d":
-                finalhand = "%sh%ss%sh%ss" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-            elif input4 == "r":
-                finalhand = "%sd%ss%sh%sc" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-            elif input4 == "t":
-                finalhand = "%sc%sd%sd%sd" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-            elif input4 == "m":
-                finalhand = "%sd%sh%sh%sc" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-            elif input4 == "l":
-                finalhand = "%ss%sc%sh%sh" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+            if input4 == 's':
+                finalhand = '%sc%sh%sh%sd' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+            elif input4 == 'd':
+                finalhand = '%sh%ss%sh%ss' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+            elif input4 == 'r':
+                finalhand = '%sd%ss%sh%sc' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+            elif input4 == 't':
+                finalhand = '%sc%sd%sd%sd' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+            elif input4 == 'm':
+                finalhand = '%sd%sh%sh%sc' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+            elif input4 == 'l':
+                finalhand = '%ss%sc%sh%sh' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
         else: #else if the first two aren't a pair, then do the suiting as below
-                if input4 == "s":
-                    finalhand = "%sh%sh%sd%sc" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-                elif input4 == "d":
-                    finalhand = "%sh%sh%ss%ss" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-                elif input4 == "t":
-                    finalhand = "%sd%sd%sd%sc" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-                elif input4 == "q":
-                    finalhand = "%sh%sh%sh%sh" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-                elif input4 == "r":
-                    finalhand = "%sd%ss%sh%sc" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-                elif input4 == "m":
-                    finalhand = "%sd%sh%sh%sc" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
-                elif input4 == "l":
-                    finalhand = "%ss%sc%sh%sh" % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+                if input4 == 's':
+                    finalhand = '%sh%sh%sd%sc' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+                elif input4 == 'd':
+                    finalhand = '%sh%sh%ss%ss' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+                elif input4 == 't':
+                    finalhand = '%sd%sd%sd%sc' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+                elif input4 == 'q':
+                    finalhand = '%sh%sh%sh%sh' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+                elif input4 == 'r':
+                    finalhand = '%sd%ss%sh%sc' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+                elif input4 == 'm':
+                    finalhand = '%sd%sh%sh%sc' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
+                elif input4 == 'l':
+                    finalhand = '%ss%sc%sh%sh' % (input0.upper(), input1.upper(), input2.upper(), input3.upper())
                 else:
-                    finalhand = "not recognized"
+                    finalhand = 'not recognized'
     #if its an 8 character input don't modify it other than the capitalization
     elif len(userinput) == 8:
-            finalhand = "%s%s%s%s%s%s%s%s" % (input0.upper(), input1.lower(), input2.upper(), input3.lower(), input4.upper(), input5.lower(), input6.upper(), input7.lower())
+            finalhand = '%s%s%s%s%s%s%s%s' % (input0.upper(), input1.lower(), input2.upper(), input3.lower(), input4.upper(), input5.lower(), input6.upper(), input7.lower())
     else:
-            finalhand = "not recognized"
+            finalhand = 'not recognized'
 
     return finalhand
 
@@ -122,21 +122,52 @@ def get_permutations(myp):
     
     return handlist
 
+def small_blind_tree(frequencies):
+  print frequencies
+  sb_action_1 = OrderedDict([
+    ('2x', frequencies['SB_2x']),
+    ('3x', frequencies['SB_3x']),
+    ('Limp', frequencies['SB_Limp']),
+    ('Fold', frequencies['SB_FoldBTN'])
+  ])
+
+  print sb_action_1
+
+  for key in sb_action_1:
+    if sb_action_1[key] > .99:
+      action = key
+      break
+    else:
+      action = 'Mix'
+  
+  if action == 'Mix':
+    for key in sb_action_1:
+      if sb_action_1[key] < .01:
+       del sb_action_1[key]
+      else:
+        pass
+
+
+
+  else:
+    pass
+
+
 def main():
   sb_files = ['50bb_SB_2x.csv', '50bb_SB_2x_4bet.csv', '50bb_SB_2x_Call3bet.csv', '50bb_SB_2x_Fold3bet.csv','50bb_SB_3x.csv', '50bb_SB_3x_4bet.csv', '50bb_SB_3x_Fold3bet.csv', '50bb_SB_3x_Call3bet.csv', '50bb_SB_FoldBTN.csv', '50bb_SB_Limp.csv', '50bb_SB_LimpCall.csv', '50bb_SB_LimpFold.csv']
   bb_files = ['50bb_BB_3Bet_2x.csv', '50bb_BB_3Bet_2x_5bet.csv', '50bb_BB_3Bet_2x_Call4bet.csv', '50bb_BB_3Bet_2x_Fold4bet.csv', '50bb_BB_3Bet_3x.csv', '50bb_BB_3bet_3x_5bet.csv', '50bb_BB_3bet_3x_Call4bet.csv', '50bb_BB_3bet_3x_Fold4bet.csv', '50bb_BB_4bet_LRR.csv', '50bb_BB_Call_2x.csv', '50bb_BB_Call_3x.csv', '50bb_BB_Call_LRR.csv', '50bb_BB_CheckLimp.csv', '50bb_BB_Fold_2x.csv', '50bb_BB_Fold_3x.csv', '50bb_BB_Fold_LRR.csv', '50bb_BB_RaiseLimp.csv']
   
-  sb_dict = get_dataframes(sb_files, "SB/")   
-  bb_dict = get_dataframes(bb_files, "BB/")
+  sb_dict = get_dataframes(sb_files, 'SB/')   
+  bb_dict = get_dataframes(bb_files, 'BB/')
 
-  # handlist = get_permutations(convertinput('AAKKd'))
+  handlist = get_permutations(convertinput('AK74d'))
 
-  # match_dict = match_dataframes(handlist, sb_dict)
-  # print match_dict
+  sb_matches = match_dataframes(handlist, sb_dict)
+  small_blind_tree(sb_matches)
 
-  sb_actions_list = []
-  for key in sb_dict:
-    print key
+  # sb_actions_list = []
+  # for key in sb_dict:
+  #   print key
 
   # print bb_actions_list
  
@@ -144,28 +175,28 @@ def main():
   
 
     # while True:
-    #    rawinput = raw_input("Input PLO Hand: ").lower()
-    #    if rawinput == "quit":
-    #         print "blade runner p: gg"
-    #         print "blade runner p: m8"
+    #    rawinput = raw_input('Input PLO Hand: ').lower()
+    #    if rawinput == 'quit':
+    #         print 'blade runner p: gg'
+    #         print 'blade runner p: m8'
     #         return
     #    else:
     #         convertedinput = convertinput(rawinput) #take raw input and convert it to a searchable plo hand
             
     #         if convertedinput == 'not recognized': #error catch
-    #             print ""
-    #             print "Oops, try again"
+    #             print ''
+    #             print 'Oops, try again'
     #         else:
     #             handlist = get_permutations(convertedinput)
     #             match_dict = match_dataframes(handlist,df_dict)
-    #             print ""
-    #             print "--------",convertedinput[0:2]+","+convertedinput[2:4]+","+convertedinput[4:6]+","+convertedinput[6:],"--------" 
-    #             print ""
-    #             print"SB:"
+    #             print ''
+    #             print '--------',convertedinput[0:2]+','+convertedinput[2:4]+','+convertedinput[4:6]+','+convertedinput[6:],'--------' 
+    #             print ''
+    #             print'SB:'
     #             small_blind_new(match_dict)
-    #             print ""
-    #             print "BB:"
+    #             print ''
+    #             print 'BB:'
     #             big_blind_new(match_dict)
-    #             print ""
+    #             print ''
 
 main() 
